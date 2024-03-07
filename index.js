b7b4f5fbacc93de69586958f075c028a6b798556
Vinayak <its07ranjanvinayak@gmail.com>
2024-03-07 11:44:43 +0530

    gitData added

--- index.js
+++ index.js
@@ -2,6 +2,41 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 
+const githubData = {
+    "login": "Vinayak1007",
+    "id": 143384814,
+    "node_id": "U_kgDOCIvg7g",
+    "avatar_url": "https://avatars.githubusercontent.com/u/143384814?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Vinayak1007",
+    "html_url": "https://github.com/Vinayak1007",
+    "followers_url": "https://api.github.com/users/Vinayak1007/followers",
+    "following_url": "https://api.github.com/users/Vinayak1007/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Vinayak1007/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Vinayak1007/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Vinayak1007/subscriptions",
+    "organizations_url": "https://api.github.com/users/Vinayak1007/orgs",
+    "repos_url": "https://api.github.com/users/Vinayak1007/repos",
+    "events_url": "https://api.github.com/users/Vinayak1007/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Vinayak1007/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 8,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2023-08-28T07:03:32Z",
+    "updated_at": "2024-02-20T04:29:27Z"
+  }
+
 app.get('/',(req,res)  =>{
     res.send('Welcome to the world of Backend ');
 });
@@ -12,6 +47,10 @@ app.get('/about',(req,res)  => {
 
 app.get('/contact',(req, res)  => {
     res.send('Contact me at :-  Nhi batayenge hum ');
+});
+
+app.get('/github',(req,res)=> {
+    res.json(githubData);
 });
 
 app.listen(process.env.PORT, () => {
